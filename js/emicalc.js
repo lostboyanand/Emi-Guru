98cc39eafb99a3accfa27af029b1bc29ef156d6a
lostboyanand <sumit.annad@gmail.com>
2024-03-06 22:01:25 +0530

    changes in emi cals js

--- js/emicalc.js
+++ js/emicalc.js
@@ -1,23 +1,104 @@
-document.getElementById("emiForm").addEventListener("submit", function(event){
-    event.preventDefault();
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureInput = document.querySelector(".loan-tenure");
 
-    //Getting the input values
-    const loanAmount = parseFloat(document.getElementById("loanAmount").value);
-    const interestRate = parseFloat(document.getElementById("interestRate").value)/100/12;
-    const loanTenureMonths = parseFloat(document.getElementById("loanTenure").value);
+const loanEMIValue = document.querySelector(".loan-emi .value");
+const totalInterestValue = document.querySelector(".total-interest .value");
+const totalAmountValue = document.querySelector(".total-amount .value");
 
-    //calculating emi
-    const emi = calculateEmi(loanAmount,interestRate,loanTenureMonths);
+const calculateBtn = document.querySelector(".calculate-btn");
 
-    //calculating total interest
-    const totalInterest =(emi * loanTenureMonths) - loanAmount;
+let loanAmount = parseFloat(loanAmountInput.value);
+let interestRate = parseFloat(interestRateInput.value);
+let loanTenure = parseFloat(loanTenureInput.value);
 
-    //redirecting after capturing the value
-    window.location.href='results.html? loanAmount = ${loanAmount}&interestRate=${interestRate * 12 * 100}&loanTenure=${loanTenureMonths}';
-});
+let interest = interestRate / 12 / 100;
 
+let myChart;
 
-    function calculateEmi(loanAmount,interestRate,loanTenureMonths){
-        const emi = loanAmount * interestRate * Math.pow(1+interestRate,loanTenureMonths)/ (Math.pow(1+interestRate,loanTenureMonths)-1);
-        return emi;
-    }
+const checkValues = () => {
+  let loanAmountValue = loanAmountInput.value;
+  let interestRateValue = interestRateInput.value;
+  let loanTenureValue = loanTenureInput.value;
+
+  let regexNumber = /^[0-9]+$/;
+  if (!loanAmountValue.match(regexNumber)) {
+    loanAmountInput.value = "10000";
+  }
+
+  if (!loanTenureValue.match(regexNumber)) {
+    loanTenureInput.value = "12";
+  }
+
+  let regexDecimalNumber = /^(\d*\.)?\d+$/;
+  if (!interestRateValue.match(regexDecimalNumber)) {
+    interestRateInput.value = "7.5";
+  }
+};
+
+const displayChart = (totalInterestPayableValue) => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Total Interest", "Principal Loan Amount"],
+      datasets: [
+        {
+          data: [totalInterestPayableValue, loanAmount],
+          backgroundColor: ["#e63946", "#14213d"],
+          borderWidth: 0,
+        },
+      ],
+    },
+  });
+};
+
+const updateChart = (totalInterestPayableValue) => {
+  myChart.data.datasets[0].data[0] = totalInterestPayableValue;
+  myChart.data.datasets[0].data[1] = loanAmount;
+  myChart.update();
+};
+
+const refreshInputValues = () => {
+  loanAmount = parseFloat(loanAmountInput.value);
+  interestRate = parseFloat(interestRateInput.value);
+  loanTenure = parseFloat(loanTenureInput.value);
+  interest = interestRate / 12 / 100;
+};
+
+const calculateEMI = () => {
+  checkValues();
+  refreshInputValues();
+  let emi =
+    loanAmount *
+    interest *
+    (Math.pow(1 + interest, loanTenure) /
+      (Math.pow(1 + interest, loanTenure) - 1));
+
+  return emi;
+};
+
+const updateData = (emi) => {
+  loanEMIValue.innerHTML = Math.round(emi);
+
+  let totalAmount = Math.round(loanTenure * emi);
+  totalAmountValue.innerHTML = totalAmount;
+
+  let totalInterestPayable = Math.round(totalAmount - loanAmount);
+  totalInterestValue.innerHTML = totalInterestPayable;
+
+  if (myChart) {
+    updateChart(totalInterestPayable);
+  } else {
+    displayChart(totalInterestPayable);
+  }
+};
+
+const init = () => {
+  let emi = calculateEMI();
+  updateData(emi);
+};
+
+init();
+
+calculateBtn.addEventListener("click", init);
